381a66a0124b6a4894322b1e303a86637d26df4c
Kiran <kiranjolisa@gmail.com>
2021-06-12 11:33:28 +0530

    UI values updated

--- src/Components/DynamicCardContents/DynamicCardContents.jsx
+++ src/Components/DynamicCardContents/DynamicCardContents.jsx
@@ -4,6 +4,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import { getDynamicData } from "../../Redux/GetDynamicData/action";
+import { NavbarRoutes } from '../NavbarRoutes'
+import { Footer } from '../Footer'
+import { Navbar } from '../Navbar';
 
 export const DynamicCardContents = () => {
     const { category, id} = useParams()
@@ -13,10 +16,14 @@ export const DynamicCardContents = () => {
     useEffect(() => {
         dispatch(getDynamicData({category, id}))
     }, [dispatch, category, id])
+    console.log(content.length === 0 ? " " : content[0])
     return <>
+     <Navbar/>
+    <NavbarRoutes />           
     <h1>Dynamic Contents Fetched!</h1>
     <h3>{content.length === 0 ? null:content[0].category}</h3>
     <h3>{content.length === 0 ? null:content[0].id}</h3>
     <h3>{content.length === 0 ? null:content[0].description}</h3>
+    <Footer />
     </>
 }
