58f83a8a1f5d2807e59ae4b68534ee95426f5692
rajatsahu18 <sahurajat78@gmail.com>
2022-12-08 17:53:24 +0530

    store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -20,4 +20,7 @@ const rootReducer = combineReducers({
 
 export const store = createStore(rootReducer,
     compose(applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+    ? a => a
+    : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()))
